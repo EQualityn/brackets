660c7518cbebdae0e0bb35b9670971709d3780b1
Egor Kalitin <egor120202@gmail.com>
2021-02-28 23:52:40 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, bracketsConfig){
+  if(str.length == 0){
+    return true;
+  }
+  for(let i = 0;i<str.length; i++){
+    for(let j= 0; j<bracketsConfig.length;j++){
+      if(bracketsConfig[j][0] == str[i] && bracketsConfig[j][1] == str[i+1]){
+       // var remove = str.splice(i, 2);
+        return check(str.slice(0, i) + str.slice(i+2), bracketsConfig);
+      }
+    }
+  }
+  return false;
+ }
